df45e2d3151d738f710561aa366835dac48649ac
CaptainFlopp3y <barrancofabian@gmail.com>
2023-07-03 20:21:03 -0600

    think I broke it

---
+++ routes/html-routes.js
@@ -0,0 +1,15 @@
+// required modules
+const router = require(`express`).Router();
+const path = require(`path`);
+
+// route for index.html
+router.get(`/`, (req, res) => {
+    res.sendFile(path.join(__dirname, `../public/index.html`));
+});
+
+// route for notes.html
+router.get(`/notes`, (req, res) => {
+    res.sendFile(path.join(__dirname, `../public/notes.html`));
+});
+
+module.exports = router;
