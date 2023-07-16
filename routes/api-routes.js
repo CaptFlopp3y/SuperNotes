63baa30187e76f5275bafb03c0eb6a8dfde43272
CaptainFlopp3y <barrancofabian@gmail.com>
2023-07-15 19:50:16 -0600

    bug fixing

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,13 +5,13 @@ const appDir = path.dirname(require.main.filename);
 
 // get request for api notes page
 router.get(`/api/notes`, async (req, res) => {
-    const dbJSON = await JSON.parse(fs.readFileSync(appDir + `Develop/db/db.json`, `utf8`));
+    const dbJSON = await JSON.parse(fs.readFileSync(appDir + `/Develop/db/db.json`, `utf8`));
     res.json(dbJSON);
 });
 
 // post request for api notes page
 router.post(`/api/notes`, (req, res) => {
-    const dbJSON = JSON.parse(fs.readFileSync(appDir + `Develop/db/db.json`, `utf8`));
+    const dbJSON = JSON.parse(fs.readFileSync(appDir + `/Develop/db/db.json`, `utf8`));
     const newNote = {
         title: req.body.title,
         text: req.body.text,
@@ -19,15 +19,15 @@ router.post(`/api/notes`, (req, res) => {
     };
     console.log(newNote);
     dbJSON.push(newNote);
-    fs.writeFileSync(appDir + `Develop/db/db.json`, JSON.stringify(dbJSON));
+    fs.writeFileSync(appDir + `/Develop/db/db.json`, JSON.stringify(dbJSON));
     res.json(dbJSON);
 });
 
 router.delete("/api/notes/:id", (req, res) => {
-    const dbJSON = JSON.parse(fs.readFileSync(appDir + "Develop/db/db.json", "utf8"));
+    const dbJSON = JSON.parse(fs.readFileSync(appDir + "/Develop/db/db.json", "utf8"));
     const noteId = req.params.id;
     const newDbJson = dbJSON.filter((note) => note.id != noteId);
-    fs.writeFileSync(appDir + "Develop/db/db.json", JSON.stringify(newDbJson));
+    fs.writeFileSync(appDir + "/Develop/db/db.json", JSON.stringify(newDbJson));
     res.json(newDbJson);
 })
 
--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const apiRoutes = require(`./routes/api-routes`);
 const htmlRoutes = require(`./routes/html-routes`);
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path')
 
 // middleware for parsing form data
+app.use(express.static(path.join(__dirname, 'Develop/public')))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(`Develop/public/`));
+// app.use(express.static(`Develop/public/`));
+app.use('/Develop', express.static('public'))
 
 // set up for the express app routes
 app.use(apiRoutes);
